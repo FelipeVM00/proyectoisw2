84cc9b08cd7bd5458ee0da184d32967febbde391
FABIO CASTELLANOS <FABIO CASTELLANOS@fabio>
2019-04-25 19:25:02 -0500

    cambios en la interfaz grafica de empresas

--- Proyecto/src/main/java/isw/proyecto/controlador/EmpresasControlador.java
+++ Proyecto/src/main/java/isw/proyecto/controlador/EmpresasControlador.java
@@ -1,5 +1,31 @@
 package isw.proyecto.controlador;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.function.Function;
+
+import com.jfoenix.controls.JFXTabPane;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.JFXTreeTableColumn;
+import com.jfoenix.controls.JFXTreeTableView;
+import com.jfoenix.controls.RecursiveTreeItem;
+import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
+
+import isw.proyecto.modelo.Residente;
+import isw.proyecto.modelo.decorator.impl.pago.Pago;
+import isw.proyecto.modelo.decorator.impl.pago.PagoAdministracion;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TreeTableColumn;
+
 public class EmpresasControlador {
 
+	
 }
