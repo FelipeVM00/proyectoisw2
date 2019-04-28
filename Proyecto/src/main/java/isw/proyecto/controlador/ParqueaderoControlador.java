42a0d9cb3b2e75e8393b6c41e91ce1e77c2cf163
Personal_Pc <Personal_Pc@Personal>
2019-04-27 23:28:06 -0500

    Controlador inicial y vista parqueadero

---
+++ Proyecto/src/main/java/isw/proyecto/controlador/ParqueaderoControlador.java
@@ -0,0 +1,96 @@
+package isw.proyecto.controlador;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXRadioButton;
+import com.jfoenix.controls.JFXTabPane;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.JFXTreeTableView;
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.TreeTableColumn;
+
+public class ParqueaderoControlador implements Initializable{
+
+	@FXML
+    private JFXTabPane pestañasPane;
+
+    @FXML
+    private JFXButton asignarParqueoIndvCarro;
+
+    @FXML
+    private TreeTableColumn<?, ?> numeroParqueoCarro;
+
+    @FXML
+    private JFXTextField nombreResidenteCarro;
+
+    @FXML
+    private JFXTextField placaVehiculoCarro;
+
+    @FXML
+    private JFXRadioButton tipoResidenteCarro;
+
+    @FXML
+    private ToggleGroup tipoVisitante;
+
+    @FXML
+    private JFXRadioButton tipoVisitanteCarro;
+
+    @FXML
+    private JFXTextField cedulaResidenteCarro;
+
+    @FXML
+    private JFXButton sorteoAleatorioCarro;
+
+    @FXML
+    private JFXTreeTableView<?> tablaTotalAsignacionCarro;
+
+    @FXML
+    private TreeTableColumn<?, ?> tablaAsignacionCarro;
+
+    @FXML
+    private JFXTextField bucarAsignacionCarro;
+
+    @FXML
+    private TreeTableColumn<?, ?> numeroParqueoMoto;
+
+    @FXML
+    private JFXTextField nombreResidenteMoto;
+
+    @FXML
+    private JFXTextField placaVehiculoMoto;
+
+    @FXML
+    private JFXRadioButton tipoResidenteMoto;
+
+    @FXML
+    private JFXRadioButton tipoVisitanteMoto;
+
+    @FXML
+    private JFXTextField cedulaResidenteMoto;
+
+    @FXML
+    private JFXButton asignarParqueoIndvMoto;
+
+    @FXML
+    private JFXButton sorteoAleatorioMoto;
+
+    @FXML
+    private JFXTreeTableView<?> tablaTotalAsignacionMoto;
+
+    @FXML
+    private TreeTableColumn<?, ?> tablaAsignacionesMoto;
+
+    @FXML
+    private JFXTextField buscarAsignacionMoto;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+}
