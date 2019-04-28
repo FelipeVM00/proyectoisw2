fd7767691c25eb1521e5f1f357c0117f79c3d196
FelipeVM00 <dvargas00@uan.edu.co>
2019-04-28 00:18:31 -0500

    Correcciones

--- Proyecto/src/main/java/isw/proyecto/controlador/FinanzasControlador.java
+++ Proyecto/src/main/java/isw/proyecto/controlador/FinanzasControlador.java
@@ -2,7 +2,13 @@ package isw.proyecto.controlador;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import org.controlsfx.control.textfield.TextFields;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXTabPane;
+import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 
 import isw.proyecto.modelo.Residente;
@@ -12,11 +18,15 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.Pane;
+import javafx.stage.StageStyle;
 
 public class FinanzasControlador implements Initializable{
 	
@@ -56,17 +66,72 @@ public class FinanzasControlador implements Initializable{
     @FXML
     private JFXTextField campoBusqueda;
     
+    @FXML
+    private JFXTextField campoNombre;
+
+    @FXML
+    private JFXTextField campoMesAPagar;
+
+    @FXML
+    private JFXDatePicker campoFecha;
+
+    @FXML
+    private JFXTextField campoValorPago;
+
+    @FXML
+    private JFXButton btnMulta;
+
+    @FXML
+    private JFXButton btnDescuento;
+
+    @FXML
+    private JFXButton btnRecargo;
+
+    @FXML
+    private JFXTextField campoValorA;
+
+    @FXML
+    private Label textoValorA;
+
+    @FXML
+    private JFXTextArea campoDetalles;
+
+    @FXML
+    private JFXButton btnOk;
+    
+    @FXML
+    private Pane paneAñadir;
+    
     private ObservableList<Pago> pagos = FXCollections.observableArrayList();
+    
+    private Boolean terminoAñadir;
+    
+    private String[] nombresResidentes = {"felipe", "andres", "david"};
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {		
 		cambiarEstilo();
+		inicializarColumnas();
+		paneAñadir.setVisible(false);
+		inicializarBuscador();
+		TextFields.bindAutoCompletion(campoNombre, nombresResidentes);
+		llenarTablaPagos();	
+	}
+
+	private void cambiarEstilo() {
+		pestañasPane.getStylesheets().add("/isw/proyecto/vista/custom.css");
+	}
+	
+	private void llenarTablaPagos() {
 		Pago pago = new PagoAdministracion(200.0, "26/04/2019", "Abril", (Residente) new Residente.ResidenteBuilder().setNombre("Felipe").setApellido("Vargas").build());
 		Pago pago2 = new PagoAdministracion(200.0, "26/04/2019", "Abril", (Residente) new Residente.ResidenteBuilder().setNombre("asdadasd").setApellido("asdasd").build());
 		Pago pago3 = new PagoAdministracion(200.0, "26/04/2019", "Abril", (Residente) new Residente.ResidenteBuilder().setNombre("Fel3241pe").setApellido("Vasdadeas").build());
 		pagos.add(pago);
 		pagos.add(pago2);
 		pagos.add(pago3);	
+	}
+	
+	private void inicializarBuscador() {
 		FilteredList<Pago> datosFiltrados = new FilteredList<>(pagos, p->true); 		
 		campoBusqueda.textProperty().addListener((observable, oldValue, newValue) -> {
 			datosFiltrados.setPredicate(pagoo -> {
@@ -84,16 +149,59 @@ public class FinanzasControlador implements Initializable{
         });		
 		SortedList<Pago> datosOrdenados = new SortedList<>(datosFiltrados);	
 		datosOrdenados.comparatorProperty().bind(tablaPagos.comparatorProperty());
-		tablaPagos.setItems(datosOrdenados);		
+		tablaPagos.setItems(datosOrdenados);
+	}
+	
+	private void inicializarColumnas() {
 		columnaID.setCellValueFactory(cell -> cell.getValue().idProperty());
 		columnaMes.setCellValueFactory(cell -> cell.getValue().mesAPagarProperty());
 		columnaValor.setCellValueFactory(cell -> cell.getValue().valorPagoProperty());
 		columnaResidente.setCellValueFactory(cell -> cell.getValue().residenteProperty());
-		
+	}
+	
+	public static void mostrarAlerta(String titulo, String msg) {
+        Alert alerta = new Alert(Alert.AlertType.WARNING);
+        alerta.initStyle(StageStyle.UTILITY);
+        alerta.setTitle("Alerta");
+        alerta.setHeaderText(titulo);
+        alerta.setContentText(msg);
+        alerta.showAndWait();
+    }
+	
+	@FXML
+	void clickBtnDescuento(ActionEvent event) {
+		terminoAñadir = false;	
+		paneAñadir.setVisible(true);
+		campoDetalles.clear();
+		if(terminoAñadir) {			
+			textoValorA.setText("Valor descuento:");
+		}else {
+			mostrarAlerta("No terminó de añadir el descuento", "Por favor termine de añadir el descuento.");
+		}
+	}	  
+
+	@FXML
+	void clickBtnMulta(ActionEvent event) {
+		paneAñadir.setVisible(true);
+		if(terminoAñadir) {			
+			textoValorA.setText("Valor multa:");
+		}else {
+			mostrarAlerta("No terminó de añadir la multa", "Por favor termine de añadir la multa.");
+		}
 	}
 
-	private void cambiarEstilo() {
-		pestañasPane.getStylesheets().add("/isw/proyecto/vista/custom.css");
-	}
-    
+	@FXML
+	void clickBtnRecargo(ActionEvent event) {
+		paneAñadir.setVisible(true);
+		if(terminoAñadir) {			
+			textoValorA.setText("Valor recargo:");
+		}else {
+			mostrarAlerta("No terminó de añadir el recargo", "Por favor termine de añadir el recargo.");
+		}
+	}    
+	
+    @FXML
+    void clickBtnOk(ActionEvent event) {
+    	
+    }
 }
--- Proyecto/src/main/java/isw/proyecto/controlador/ParqueaderoControlador.java
+++ Proyecto/src/main/java/isw/proyecto/controlador/ParqueaderoControlador.java
@@ -10,7 +10,11 @@ import com.jfoenix.controls.JFXRadioButton;
 import com.jfoenix.controls.JFXTabPane;
 import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.controls.JFXTreeTableView;
+
+import isw.proyecto.modelo.Parqueadero;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.TreeTableColumn;
 
@@ -20,7 +24,7 @@ public class ParqueaderoControlador implements Initializable{
     private JFXTabPane pestañasPane;
 
     @FXML
-    private TreeTableColumn<?, ?> numeroParqueoCarro;
+    private JFXTextField numeroParqueoCarro;
 
     @FXML
     private JFXTextField nombreResidenteCarro;
@@ -47,16 +51,19 @@ public class ParqueaderoControlador implements Initializable{
     private JFXButton sorteoAleatorioCarro;
 
     @FXML
-    private JFXTreeTableView<?> tablaTotalAsignacionCarro;
+    private TableView<Parqueadero> tablaAsignacionCarros;
 
     @FXML
-    private TreeTableColumn<?, ?> tablaAsignacionCarro;
+    private TableColumn<Parqueadero, String> columnaNumParqueaderoCar;
+
+    @FXML
+    private TableColumn<Parqueadero, String> columnaResidenteAsignCar;
 
     @FXML
     private JFXTextField bucarAsignacionCarro;
 
     @FXML
-    private TreeTableColumn<?, ?> numeroParqueoMoto;
+    private JFXTextField numeroParqueoMoto;
 
     @FXML
     private JFXTextField nombreResidenteMoto;
@@ -80,10 +87,13 @@ public class ParqueaderoControlador implements Initializable{
     private JFXButton sorteoAleatorioMoto;
 
     @FXML
-    private JFXTreeTableView<?> tablaTotalAsignacionMoto;
+    private TableView<Parqueadero> tablaAsignacionMot;
 
     @FXML
-    private TreeTableColumn<?, ?> tablaAsignacionesMoto;
+    private TableColumn<Parqueadero, String> columnaNumParqueaderoMot;
+
+    @FXML
+    private TableColumn<Parqueadero, String> columnaResidenteAsignMot;
 
     @FXML
     private JFXTextField buscarAsignacionMoto;
