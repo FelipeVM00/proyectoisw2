9ccf8aa333f38c8668a5cd675287430e6ef4c6db
Personal_Pc <Personal_Pc@Personal>
2019-04-29 12:21:14 -0500

    Vista y controlador parqueadero adiciones

--- Proyecto/src/main/java/isw/proyecto/controlador/ParqueaderoControlador.java
+++ Proyecto/src/main/java/isw/proyecto/controlador/ParqueaderoControlador.java
@@ -20,83 +20,94 @@ import javafx.scene.control.TreeTableColumn;
 
 public class ParqueaderoControlador implements Initializable{
 
+	
+	/**
+	 * declarando Botones
+	 */
+	@FXML
+    private JFXButton asignarParqueoIndvCarro;
+	@FXML
+    private JFXButton sorteoAleatorioCarro;
+	@FXML
+    private JFXButton asignarParqueoIndvMoto;
+    @FXML
+    private JFXButton sorteoAleatorioMoto;
+	
+    
+    /**
+	 * declarando TabPane
+	 */
 	@FXML
     private JFXTabPane pestañasPane;
 
+	
+	/**
+	 * declarando TextField
+	 */
     @FXML
     private JFXTextField numeroParqueoCarro;
-
     @FXML
     private JFXTextField nombreResidenteCarro;
-
     @FXML
     private JFXTextField placaVehiculoCarro;
-
-    @FXML
-    private JFXRadioButton tipoResidenteCarro;
-
-    @FXML
-    private ToggleGroup tipoVisitante;
-
-    @FXML
-    private JFXRadioButton tipoVisitanteCarro;
-
     @FXML
     private JFXTextField cedulaResidenteCarro;
-
-    @FXML
-    private JFXButton asignarParqueoIndvCarro;
-
-    @FXML
-    private JFXButton sorteoAleatorioCarro;
-
-    @FXML
-    private TableView<Parqueadero> tablaAsignacionCarros;
-
-    @FXML
-    private TableColumn<Parqueadero, String> columnaNumParqueaderoCar;
-
-    @FXML
-    private TableColumn<Parqueadero, String> columnaResidenteAsignCar;
-
     @FXML
     private JFXTextField bucarAsignacionCarro;
-
     @FXML
     private JFXTextField numeroParqueoMoto;
-
     @FXML
     private JFXTextField nombreResidenteMoto;
-
     @FXML
     private JFXTextField placaVehiculoMoto;
-
     @FXML
-    private JFXRadioButton tipoResidenteMoto;
-
-    @FXML
-    private JFXRadioButton tipoVisitanteMoto;
-
+    private JFXTextField buscarAsignacionMoto;
     @FXML
     private JFXTextField cedulaResidenteMoto;
-
+    
+    
+    /**
+	 * declarando RadioButton
+	 */
     @FXML
-    private JFXButton asignarParqueoIndvMoto;
-
+    private JFXRadioButton tipoResidenteCarro;
     @FXML
-    private JFXButton sorteoAleatorioMoto;
-
+    private JFXRadioButton tipoVisitanteCarro;
+    @FXML
+    private JFXRadioButton tipoResidenteMoto;
+    @FXML
+    private JFXRadioButton tipoVisitanteMoto;
+    
+    
+    /**
+	 * declarando TableView
+	 */
+    @FXML
+    private TableView<Parqueadero> tablaAsignacionCarros;
     @FXML
     private TableView<Parqueadero> tablaAsignacionMot;
 
+    
+    /**
+	 * declarando TableColumn
+	 */
+    @FXML
+    private TableColumn<Parqueadero, String> columnaNumParqueaderoCar;
+    @FXML
+    private TableColumn<Parqueadero, String> columnaResidenteAsignCar;
     @FXML
     private TableColumn<Parqueadero, String> columnaNumParqueaderoMot;
-
     @FXML
     private TableColumn<Parqueadero, String> columnaResidenteAsignMot;
-
     @FXML
-    private JFXTextField buscarAsignacionMoto;
+    private TableColumn<Parqueadero, String> columnaPlacaCar;
+    @FXML
+    private TableColumn<Parqueadero, String> columnaPlacaMot;
+    
+    @FXML
+    private ToggleGroup tipoVisitante;
+
+    
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
