92197ea8997510ce772923c4119877b7c58f9cf2
FelipeVM00 <dvargas00@uan.edu.co>
2019-04-30 01:50:02 -0500

    Modificacion scripts bd

--- Proyecto/src/db/ddl/createTablesDb.sql
+++ Proyecto/src/db/ddl/createTablesDb.sql
@@ -1,4 +1,6 @@
 DROP TABLE IF EXISTS pagos;
+DROP TABLE IF EXISTS empresas;
+DROP TABLE IF EXISTS parqueaderos;
 
 CREATE TABLE pagos (
 	id int NOT NULL AUTO_INCREMENT,
@@ -9,3 +11,22 @@ CREATE TABLE pagos (
 	PRIMARY KEY (id)
 ) ENGINE=INNODB;
 
+CREATE TABLE empresas (
+	nombre varchar(255) NOT NULL,
+    tipoEmpresa varchar(255) NOT NULL,
+    numeroContrato varchar(255) NOT NULL,
+    telefono int NOT NULL,
+    fechaInicio varchar(255) NOT NULL,
+    fechaTerminacion varchar(255) NOT NULL,
+    valorContrato double NOT NULL,
+	PRIMARY KEY (numeroContrato)
+) ENGINE=INNODB;
+
+CREATE TABLE parqueaderos (
+	numero int NOT NULL,
+    numeroPiso int NULL,
+    ocupante varchar(255) NOT NULL,
+    visitante varchar(266) NULL,
+    placa varchar(6) NOT NULL,
+	PRIMARY KEY (numero)
+) ENGINE=INNODB;
--- Proyecto/src/db/dml/insertDataToDb.sql
+++ Proyecto/src/db/dml/insertDataToDb.sql
@@ -3,3 +3,15 @@ INSERT INTO pagos (valorPago, fechaPago, mesAPagar, residente) VALUES (20000.0, 
 INSERT INTO pagos (valorPago, fechaPago, mesAPagar, residente) VALUES (20000.0, '04/04/2019', 'Abril', 'Pedro Perez');
 INSERT INTO pagos (valorPago, fechaPago, mesAPagar, residente) VALUES (20000.0, '04/04/2019', 'Abril', 'Fabio Ivan');
 INSERT INTO pagos (valorPago, fechaPago, mesAPagar, residente) VALUES (20000.0, '04/04/2019', 'Abril', 'Luis Diego');
+
+INSERT INTO empresas (nombre, tipoEmpresa, numeroContrato, telefono, fechaInicio, fechaTerminacion, valorContrato)
+		VALUES('Superior S', 'Vigilancia', '23T21', 3122545, '03/05/2018', '03/05/2090', 130000000.0);
+INSERT INTO empresas (nombre, tipoEmpresa, numeroContrato, telefono, fechaInicio, fechaTerminacion, valorContrato)
+		VALUES('Limpion r', 'Aseo', '23T22', 3128524, '03/05/2019', '03/05/2020', 50000000.0);
+INSERT INTO empresas (nombre, tipoEmpresa, numeroContrato, telefono, fechaInicio, fechaTerminacion, valorContrato)
+		VALUES('Repareme este', 'Mantenimiento', '23T23', 3128749, '09/09/2018', '05/09/2019', 15000000.0);        
+        
+INSERT INTO parqueaderos (numero, numeroPiso, ocupante, visitante, placa) VALUES (1, 1, 'David Felipe', NULL, 'ARP531');
+INSERT INTO parqueaderos (numero, numeroPiso, ocupante, visitante, placa) VALUES (2, 1, 'Andres Felipe', NULL, 'DRE791');
+INSERT INTO parqueaderos (numero, numeroPiso, ocupante, visitante, placa) VALUES (3, 1, 'Pedro Perez', NULL, 'OER753');
+INSERT INTO parqueaderos (numero, numeroPiso, ocupante, visitante, placa) VALUES (4, 1, 'Fabio Ivan', NULL, 'POI741');
